850203eb29b96400af6c9da710535653bd7aff47
Ben Blackman <bblkmn5@gmail.com>
2017-12-02 23:07:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,27 +1,13 @@
 class PigLatinizer
 
   def piglatinize(word)
-    if word =~ (/\A[aeiou]/i)
-    word = word + 'ay'
-  elsif word =~ (/\A[^aeiou]/i)
-    match = /\A[^aeiou]/i.match(word)
-    word = match.post_match + match.to_s + 'ay'
-  end
-  word
-    # alpha = ('a'..'z').to_a
-    # vowels = ['a', 'e', 'i', 'o', 'u']
-    # consonants = alpha - vowels
-    #   if non_words.include?(word)
-    #     word << "way"
-    #   elsif vowels.include? word[0]
-    #     word << "ay"
-    #   else
-    #     while !vowels.include? word[0]
-    #       consonants << word[0]
-    #       word = word.split("")[1..-1].join
-    #   end
-    #     word + consonants + "ay"
-    # end
+    if /[aeiou]/ == word[0].downcase
+      "#{word}way"
+    else
+      first = word.split(/[aeiou]).first
+      word = word.sub(first,"")
+      "#{word}#{first}ay"
+    end
   end
 
   def to_pig_latin(string)
