62920365d16817cf18f93f038fc1f7fce3effe29
Ben Blackman <bblkmn5@gmail.com>
2017-12-02 22:49:26 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
 
   def piglatinize(word)
-    non_words = ['i', 'me', 'to', 'too', 'a', 'an', 'in', 'and', 'on']
+    non_words = ['I', 'me', 'to', 'too', 'a', 'an', 'in', 'and', 'on']
       vowels = ['a', 'e', 'i', 'o', 'u']
       if non_words.include?(word)
         word
