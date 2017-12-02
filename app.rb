5c090c79fa9ce69023ba969c1884c79b0e45f333
Ben Blackman <bblkmn5@gmail.com>
2017-12-02 23:19:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.to_pit_latin(params[:user_phrase])
+    @piglatin = pl.to_pig_latin(params[:user_phrase])
 
     erb :results
   end
