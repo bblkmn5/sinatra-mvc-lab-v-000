8c2f1a695fa14419e7924484cee1226aea264492
Ben Blackman <bblkmn5@gmail.com>
2017-12-02 23:00:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,27 @@
 class PigLatinizer
 
   def piglatinize(word)
-    non_words = ['I', 'me', 'to', 'too', 'a', 'an', 'in', 'and', 'on']
-    alpha = ('a'..'z').to_a
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    consonants = alpha - vowels
-      if non_words.include?(word)
-        word << "way"
-      elsif vowels.include? word[0]
-        word << "ay"
-      else
-        while !vowels.include? word[0]
-          consonants << word[0]
-          word = word.split("")[1..-1].join
-      end
-        word + consonants + "ay"
-    end
+    if word =~ (/\A[aeiou]/i)
+    word = word + 'ay'
+  elsif word =~ (/\A[^aeiou]/i)
+    match = /\A[^aeiou]/i.match(word)
+    word = match.post_match + match.to_s + 'ay'
+  end
+  word
+    # alpha = ('a'..'z').to_a
+    # vowels = ['a', 'e', 'i', 'o', 'u']
+    # consonants = alpha - vowels
+    #   if non_words.include?(word)
+    #     word << "way"
+    #   elsif vowels.include? word[0]
+    #     word << "ay"
+    #   else
+    #     while !vowels.include? word[0]
+    #       consonants << word[0]
+    #       word = word.split("")[1..-1].join
+    #   end
+    #     word + consonants + "ay"
+    # end
   end
 
   def to_pig_latin(string)
