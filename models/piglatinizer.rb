06e5c7003354f50fd1a0db9544dfeb3eabaf700d
Ben Blackman <bblkmn5@gmail.com>
2017-12-02 22:35:54 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def piglatinize(word)
+      vowels = ['a', 'e', 'i', 'o', 'u']
+
+      if vowels.include? word[0]
+        word + "ay"
+      else
+        constants = ""
+        while !vowels.include? word[0]
+          constants << word[0]
+          word = word.split("")[1..-1].join
+      end
+        word + constants + "ay"
+    end
+  end
+
+  def to_pig_latin(string)
+    string.split.map {|word| piglatinize(word)}.join(" ")
+  end
